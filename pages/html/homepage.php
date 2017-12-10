6ca920205a14c246423dcf808c60ffd1b6256da7
sachin phpstorm <sachin.jose9@gmail.com>
2017-12-10 03:31:08 -0500

    added styling for homepage

--- pages/html/homepage.php
+++ pages/html/homepage.php
@@ -1,31 +1,61 @@
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
-
     <title>The Task List</title>
-    <meta name="description" content="">
+    <meta name="description" content="Homepage for wsd final project: ">
     <meta name="author" content="Sachin Mathew Jose">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="pages/css/homepage.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/dist/bootsnipp.min.css">
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 </head>
 
 <body>
-<h1>
-    <?php
-    //this how to print some data;
-    echo $data['site_name'];
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="form-wrap">
+                    <h1>Log in with your email account</h1>
+                    <form role="form" action="index.php?page=accounts&action=login" method="POST" id="login-form" autocomplete="off">
+                        <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                        </div>
+                        <div class="form-group">
+                            <label for="key" class="sr-only">Password</label>
+                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="checkbox">
+                            <span class="character-checkbox" onclick="showPassword()"></span>
+                            <span class="label">Show password</span>
+                        </div>
+                        <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in">
+                    </form>
+                    <p class="register">New user?<a href="index.php?page=accounts&action=register" data-toggle="modal" data-target=".forget-modal">Register</></p>
+                    <hr>
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
 
-    ?> </h1>
-
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
-
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p><a href="index.php?page=accounts&action=all">Show All Accounts</a></p>
+                <p><a href="index.php?page=tasks&action=all">Show All Tasks</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<h1></h1>
+<h1></h1>
+<!--
 <form action="index.php?page=accounts&action=login" method="POST">
     <div class="container">
         <label><b>Username</b></label>
@@ -35,8 +65,7 @@
         <button type="submit">Login</button>
     </div>
 </form>
-
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
-<script src="js/scripts.js"></script>
+-->
+<script src="pages/script/homepage.js"></script>
 </body>
 </html>
